dcfc7a58832f3488160a3392939aefa2b641920d
ConorCurry <conorcurry@gmail.com>
2016-02-18 23:41:32 -0500

    Implemented AES/Blowfish tests, both are working

--- CryptoTests.java
+++ CryptoTests.java
@@ -1,26 +1,37 @@
-import org.bouncycastle.jce.provider.*
-import javax.crypto.*
+import org.bouncycastle.jce.provider.*;
+import javax.crypto.*;
+import javax.crypto.spec.IvParameterSpec;
+import javax.crypto.spec.SecretKeySpec;
+import java.security.*;
+import java.util.*;
 
-public Class CryptoTests {
+public class CryptoTests {
     public static void main(String[] args) {
-	Scanner keyboard = new Scanner();
+	Scanner keyboard = new Scanner(System.in);
 	System.out.println("Please enter the string you would like to encrypt");
 	String msg = keyboard.nextLine();
 	Security.addProvider(new BouncyCastleProvider());
+	System.out.println("---AES---");
+	testAES(msg);
+	System.out.println("---Blowfish---");
+	testBlowfish(msg);
     }
-    public void testAES(String msg) {
-	Key key;
-	KeyGenerator keyGen;
-	Cipher cipher;
-	String ciphertext, plaintext;
+    public static void testAES(String msg) {
+	SecretKeySpec key = null;
+	IvParameterSpec iv = null;
+	KeyGenerator keyGen = null;
+	Cipher cipher = null;
+	byte[] ciphertext = null;
+	byte[] plaintext = null;
 
 	//Generate 128 bit key
-	try {
+	try { 
 	    keyGen = KeyGenerator.getInstance("AES", "BC");
 	    keyGen.init(128, new SecureRandom());
 
-	    key = keyGen.generateKey();
+	    key = (SecretKeySpec)keyGen.generateKey(); 
 	} catch (Exception e) {
+	    System.err.print("In key generation: ");
 	    System.err.println(e);
 	    System.exit(1);
 	}
@@ -30,30 +41,77 @@ public Class CryptoTests {
 	    //use padding for AES CBC mode.
 	    cipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
 	    cipher.init(Cipher.ENCRYPT_MODE, key);
+	    iv = new IvParameterSpec(cipher.getIV());
 
-	    ciphertext = String(cipher.doFinal(msg.getBytes()));
-	    System.out.println("Encrypted Ciphertext: " + ciphertext);
+	    ciphertext = cipher.doFinal(msg.getBytes());
+	    System.out.println("Encrypted Ciphertext: " + new String(ciphertext));
 	} catch (Exception e) {
+	    System.err.print("In encryption: ");
 	    System.err.println(e);
 	    System.exit(1);
 	}
 
 	//decrypt ciphertext
 	try {
-	    cipher.init(Cipher.DECRYPT_MODE, key);
+	    IvParameterSpec ivParameterSpec = new IvParameterSpec(key.getEncoded());
+	    cipher.init(Cipher.DECRYPT_MODE, key, iv);
 	    
-	    plaintext = String(cipher.doFinal(msg.getBytes()));
-	    System.out.println("Decrypted Plaintext: " + plaintext);
+	    plaintext = cipher.doFinal(ciphertext);
+	    System.out.println("Decrypted Plaintext: " + new String(plaintext));
 	} catch (Exception e) {
+	    System.err.print("In decryption: ");
 	    System.err.println(e);
 	    System.exit(1);
 	}
+
     }
-    public void testBlowfish(String msg) {
+    public static void testBlowfish(String msg) {
+	SecretKeySpec key = null;
+	IvParameterSpec iv = null;
+	KeyGenerator keyGen = null;
+	Cipher cipher = null;
+	byte[] ciphertext = null;
+	byte[] plaintext = null;
+
 	//Generate 128 bit key
+	try { 
+	    keyGen = KeyGenerator.getInstance("Blowfish", "BC");
+	    keyGen.init(128, new SecureRandom());
+
+	    key = (SecretKeySpec)keyGen.generateKey(); 
+	} catch (Exception e) {
+	    System.err.print("In key generation: ");
+	    System.err.println(e);
+	    System.exit(1);
+	}
+
 	//encrypt msg
+	try {
+	    //use padding for AES CBC mode.
+	    cipher = Cipher.getInstance("Blowfish/CBC/PKCS5Padding");
+	    cipher.init(Cipher.ENCRYPT_MODE, key);
+	    iv = new IvParameterSpec(cipher.getIV());
+
+	    ciphertext = cipher.doFinal(msg.getBytes());
+	    System.out.println("Encrypted Ciphertext: " + new String(ciphertext));
+	} catch (Exception e) {
+	    System.err.print("In encryption: ");
+	    System.err.println(e);
+	    System.exit(1);
+	}
+
 	//decrypt ciphertext
-	//print plaintext
+	try {
+	    IvParameterSpec ivParameterSpec = new IvParameterSpec(key.getEncoded());
+	    cipher.init(Cipher.DECRYPT_MODE, key, iv);
+	    
+	    plaintext = cipher.doFinal(ciphertext);
+	    System.out.println("Decrypted Plaintext: " + new String(plaintext));
+	} catch (Exception e) {
+	    System.err.print("In decryption: ");
+	    System.err.println(e);
+	    System.exit(1);
+	}
     }
     public void testRSA(String msg) {
 	//Generate keypair
